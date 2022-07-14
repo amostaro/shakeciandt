07f19dcc9b7cfb9c020b64e8cd884cb7185cadb1
amostaro <amostaro@ciandt.com>
2022-07-14 08:20:16 -0300

    feature/iss-005 - regra 2

--- src/main/java/produto/TipoTamanho.java
+++ src/main/java/produto/TipoTamanho.java
@@ -2,6 +2,7 @@ package produto;
 
 public enum TipoTamanho {
     //IMPLEMENTE A LOGICA DO ENUM
+    // adição dos fatores de multiplicação - REGRA 2
     P(1),M(1.3),G(1.5);
     public final double multiplicador;
 
